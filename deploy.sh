ee5b79f547f43dc328fa5669aff7db41934c5652
景粤斌 <jingyuebin@hi7.cn>
2026-08-25 17:24:26 +0800

    fix(deploy): use git reset --hard origin/master in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/ubuntu/homebuy && git pull origin master && npm run build && pm2 restart prediction-api
+cd /home/ubuntu/homebuy && git fetch origin master && git reset --hard origin/master && npm run build && pm2 restart prediction-api
